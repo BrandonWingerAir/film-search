6c89eece95067fe77a8d45f2193dcd666beeb342
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-05-04 22:12:39 -0400

    create css styling and search form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { useEffect } from "react";
 
-const API_URL = 'http://www.omdbapi.com?apikey=YOUR_API_KEY';
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=API_KEY';
 
 const App = () => {
     const searchFilms = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data);
+        console.log(data.Search);
     }
 
     useEffect(() => {
@@ -15,7 +18,22 @@ const App = () => {
     }, []);
 
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>Find A Film</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search..."
+                    value=""
+                    onChange={() => {}}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+        </div>
     );
 }
 
