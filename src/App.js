2be9e4c63d8fb69c54d9abaeda8a0f7be2ff2be1
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-05-04 21:45:43 -0400

    access api search in console log

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import { useEffect } from "react";
+
+const API_URL = 'http://www.omdbapi.com?apikey=YOUR_API_KEY';
 
 const App = () => {
+    const searchFilms = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+    useEffect(() => {
+        searchFilms('Test');
+    }, []);
+
     return (
         <h1>App</h1>
     );
